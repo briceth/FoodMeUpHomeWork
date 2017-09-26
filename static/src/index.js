86482dd5b4e8d01df395a42dc6293b0180f5cd61
Brice Tessier Huort <tessierhuort@gmail.com>
2017-09-27 00:01:27 +0200

    make the boilerplate

--- static/src/index.js
+++ static/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
+import App from './views/App';
 
 export const createStoreWithMiddleware = createStore(
   reducers,
@@ -18,29 +19,7 @@ export const createStoreWithMiddleware = createStore(
 
 import { injectGlobal } from 'styled-components';
 
-injectGlobal`
-  @import url('https://necolas.github.io/normalize.css/7.0.0/normalize.css');
-  @import url('https://fonts.googleapis.com/css?family=Titillium+Web:400,600');
-  html {
-    text-rendering: optimizeLegibility;
-    font-feature-settings: "pnum";
-    font-variant-numeric: proportional-nums;
-    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-    font-size: 16px;
-    font-weight: 400;
-    overflow-x: hidden;
-    line-height: 1.8;
-    color: white;
-  }
-
-  body {
-    margin: 0;
-  }
-
-  *{ box-sizing: border-box }
-`;
-
-import App from './views/App';
+injectGlobal``;
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware}>
